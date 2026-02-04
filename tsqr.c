70948066fd741b5d4fc57de21c519efae05c952e
Kevin Knights <kevinknights29@hotmail.com>
2026-02-04 11:13:01 +0000

    fix: updated rank and size retrieval from MPI

--- tsqr.c
+++ tsqr.c
@@ -15,8 +15,8 @@ int main(int argc, char *argv[]) {
 	MPI_Init(&argc, &argv);
 	int rank = 0;
 	int size = 0;
-	MPI_Comm_rank(&rank, MPI_COMM_WORLD);
-	MPI_Comm_size(&size, MPI_COMM_WORLD);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
 	// Check command line arguments
 	if (argc != 4) {
