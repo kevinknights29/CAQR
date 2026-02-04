b8b5ae6e0b683fec1023474ad4f6f116b2495831
Kevin Knights <kevinknights29@hotmail.com>
2026-02-04 11:08:19 +0000

    refactor: resolved memory leak and minor changes

--- matrix_generator.c
+++ matrix_generator.c
@@ -4,7 +4,7 @@
 #include <time.h>
 
 
-int main(int argc, char *argv[]) {
+int main(const int argc, char *argv[]) {
 	// Takes two parameters m and n
 	if (argc != 3) {
 		fprintf(stderr, "Usage: %s <m> <n>\n", argv[0]);
@@ -14,19 +14,18 @@ int main(int argc, char *argv[]) {
 	char *end = NULL;
 	const long m = strtol(argv[1], &end, 10);
 	if ((argv[1] == end) || (*end != '\0')) {
-		fprintf(stderr, "Unable to parse m from comand line...\n");
+		fprintf(stderr, "Unable to parse m from command line...\n");
 		return EXIT_FAILURE;
 	}
 
 	const long n = strtol(argv[2], &end, 10);
 	if ((argv[2] == end) || (*end != '\0')) {
-		fprintf(stderr, "Unable to parse n from comand line...\n");
+		fprintf(stderr, "Unable to parse n from command line...\n");
 		return EXIT_FAILURE;
 	}
 
 	// Generates a matrix of size (m x n) filled with random numbers
-	srandom((unsigned) time(NULL));	
-	double scaling_factor  = 100.0;
+	srandom((unsigned) time(NULL));
 	double *matrix = malloc((long unsigned) (m * n) * sizeof(*matrix));
 	if (matrix == NULL) {
 		fprintf(stderr, "Unable to allocate memory for matrix...\n");
@@ -34,6 +33,7 @@ int main(int argc, char *argv[]) {
 	}
 	for (size_t i = 0; i < (size_t) m; i++) {
 		for (size_t j = 0; j < (size_t) n; j++) {
+			const double scaling_factor  = 100.0;
 			matrix[((size_t) m * i) + j] = scaling_factor * ((double) random() / RAND_MAX);
 		}
 	}
@@ -44,6 +44,7 @@ int main(int argc, char *argv[]) {
 	FILE *file = fopen(filename, "w");
 	if (file == NULL) {
 		fprintf(stderr, "Unable to open: %s...\n", filename);
+		free(matrix);
 		return EXIT_FAILURE;
 	}
 	for (size_t i = 0; i < (size_t) m; i++) {
